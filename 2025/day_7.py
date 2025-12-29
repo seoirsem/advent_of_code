6b73543870d71574a589eed60d5fb97e5e49ee34
Seoirse Murray <murray@seoirse.net>
2025-12-29 14:19:53 +0000

    day 8

--- 2025/day_7.py
+++ 2025/day_7.py
@@ -26,12 +26,36 @@ class SplitterGrid:
         for i, object in enumerate(input[0]):
             if object == "S":
                 self.beams.append((i,0))
+        self.start = self.beams[0]
+        self.beam_len_map = {}
 
     def get_at(self, x, y):
         if x < 0 or x >= self.lenx or y < 0 or y >= self.leny:
             return None
         return self.input[y][x]
-    
+
+    def backstep(self, beam_pos):
+        if beam_pos in self.beam_len_map:
+            return self.beam_len_map[beam_pos]
+        next_pos = (beam_pos[0], beam_pos[1]+1)
+        if self.get_at(*next_pos) == ".":
+            number = self.backstep(next_pos)
+            self.beam_len_map[next_pos] = number
+            return number
+        elif self.get_at(*next_pos) == "^":
+            pos_l = (beam_pos[0]-1, beam_pos[1])
+            pos_r = (beam_pos[0]+1, beam_pos[1])
+            num_l = self.backstep(pos_l)
+            self.beam_len_map[pos_l] = num_l
+            num_r = self.backstep(pos_r)
+            self.beam_len_map[pos_r] = num_r
+            return num_l + num_r
+        else:
+            return 1
+
+    def total_from_start(self):
+        return self.backstep(self.start)
+
     def step_beams(self):
         splits = 0
         visited = set()
@@ -46,8 +70,8 @@ class SplitterGrid:
             elif self.get_at(*next_step) == "^":
                 visited.add(next_step)
                 splits+=1
-                new_beams.add((beam[0]-1, beam[1]+1))
-                new_beams.add((beam[0]+1, beam[1]+1))
+                new_beams.add((beam[0]-1, beam[1]))
+                new_beams.add((beam[0]+1, beam[1]))
         self.beams = new_beams
         return splits
 
@@ -66,6 +90,7 @@ def main(test: bool=True):
 
     grid = SplitterGrid(input)
     print(f"The answer to Part 1 is {grid.step_all()}")
+    print(f"The answer to Part 2 is {grid.total_from_start()}")
 
 def parse_args():
     parser = argparse.ArgumentParser()
--- 2025/day_8.py
+++ 2025/day_8.py
@@ -1,15 +1,105 @@
 import argparse
 
-test_input = """
-"""
+test_input = """162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689"""
+
+def distance(a, b):
+    return sum([(a[i]-b[i])**2 for i in range(3)])
+class Locations:
+
+    def __init__(self, input, is_test):
+        if is_test:
+            number = 10
+        else:
+            number = 1000
+        self.boxes = [[int(y) for y in x.split(",")] for x in input]
+        distances = {}
+        for i in range(len(self.boxes)):
+            for j in range(i+1, len(self.boxes)):
+                distances[distance(self.boxes[i], self.boxes[j])] = (i,j)
+        distance_vals = list(distances.keys())
+        distance_vals.sort()
+        lowest_keys = distance_vals[:number]
+        self.lowest_pairs = [distances[x] for x in lowest_keys]
+        self.all_pairs = [distances[x] for x in distance_vals]
+        self.num_boxes = len(input)
+
+    def calculate_circuits(self):
+        circuits = []
+        for new_pair in self.lowest_pairs:
+            combine_idx = []
+            new_circuits = []
+            for i, circuit in enumerate(circuits):
+                not_in = True
+                for item in new_pair:
+                    if item in circuit:
+                        combine_idx.append(i)
+                        not_in = False
+                        break
+                if not_in:
+                    new_circuits.append(circuit)
+            new_set = set(new_pair)
+            for combine in combine_idx:
+                new_set.update(circuits[combine])
+            new_circuits.append(new_set)
+            circuits = new_circuits
+        circuit_sizes = [len(x) for x in circuits]
+        circuit_sizes.sort()
+        return circuit_sizes[-1]*circuit_sizes[-2]*circuit_sizes[-3]
+
+    def calculate_all_circuits(self):
+        circuits = []
+        for new_pair in self.all_pairs:
+            combine_idx = []
+            new_circuits = []
+            for i, circuit in enumerate(circuits):
+                not_in = True
+                for item in new_pair:
+                    if item in circuit:
+                        combine_idx.append(i)
+                        not_in = False
+                        break
+                if not_in:
+                    new_circuits.append(circuit)
+            new_set = set(new_pair)
+            for combine in combine_idx:
+                new_set.update(circuits[combine])
+            new_circuits.append(new_set)
+            circuits = new_circuits
+            # print(len(circuits), sum(len(x) for x in circuits))
+            if len(circuits) == 1 and len(circuits[0]) == self.num_boxes:
+                break
+        return self.boxes[new_pair[0]][0]*self.boxes[new_pair[1]][0]
+
+
 
 def main(test: bool=True):
     if test:
         input = test_input.split("\n")
     else:
-        input = [x.strip() for x in open("2025/data_x.txt")]
+        input = [x.strip() for x in open("2025/data_8.txt")]
 
-    print(input)
+    loc = Locations(input, test)
+    print(f"The answer to part 1 is {loc.calculate_circuits()}")
+    print(f"The answer to part 2 is {loc.calculate_all_circuits()}")
 
 def parse_args():
     parser = argparse.ArgumentParser()
